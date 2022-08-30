82c474c83652c74e0fd94e4dab12dd041aea8ac5
Shahim Safin <safinsahim@gmail.com>
2022-08-31 00:36:08 +0300

    refactoring

--- src/utils/makeAuthorization.js
+++ src/utils/makeAuthorization.js
@@ -1,17 +1,17 @@
 import sendRequest from "./sendRequest";
+import { BASIC_URL } from "../environmentConstants";
 
-const BASIC_URL = 'https://46.138.244.16:6321/api/v1/'
-
-function makeAuthorization (username, password) {
-  return (
-    sendRequest(BASIC_URL + 'auth/login/', 'POST', JSON.stringify({ username, password }))
-    .then(responce => {
-      if (responce.token) {
-        sessionStorage.setItem('Token', `Token ${responce.token}`);
-      }
-    return responce.token
-    })
-  )
+function makeAuthorization(username, password) {
+  return sendRequest(
+    BASIC_URL + "auth/login/",
+    "POST",
+    JSON.stringify({ username, password })
+  ).then((responce) => {
+    if (responce.token) {
+      sessionStorage.setItem("Token", `Token ${responce.token}`);
+    }
+    return responce.token;
+  });
 }
 
 export default makeAuthorization;
